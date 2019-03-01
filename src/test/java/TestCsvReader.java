c7613e18ec04030f64ea44c8c757118d49297d27
dommeti ramya <dommetiramya@gmail.com>
2019-03-01 23:10:21 +0100

    TEstcase in Test

--- src/test/java/TestCsvReader.java
+++ src/test/java/TestCsvReader.java
@@ -1,9 +1,14 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCsvReader {
 
     @Test
     public void testMethod() {
-
+        TestClass t = new TestClass ( );
+        Integer total=t.getName ( 2, 2 );
+        Assertions.assertNotNull ( total);
+        Assertions.assertEquals (total,2+2 );
     }
+
 }
